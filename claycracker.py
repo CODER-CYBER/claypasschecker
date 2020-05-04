14590d416c502a1064f867249b57170dd434b571
CODER-CYBER <55795299+CODER-CYBER@users.noreply.github.com>
2020-05-04 21:44:34 +0200

    Add files via upload

--- claycracker.py
+++ claycracker.py
@@ -1,3 +1,39 @@
+print(''''
+         `+y/sNMMd.hh/oshMmNNNhyooo++yyhhhyysssooosssyyhhhhs+++oosymNMNmm:ymyhNmyNMNy/y+`          
+         -yo+mMMMMMs:sdNmmMmhso/+/osddhso/::--.......--:/+oshdh++/+/ooydNNdyhNmMMNhodMm++y-         
+        /y/yNMMMMMMN/sMMNmh+o+o+osdho+:-.``             ``.-/+shds+/oo++ydNNdhMNy/+dyyNMy/y/        
+       +y/dMMMMMMMMdmmMms+//+//ody+:-``                     `.-/ohd+///+++ohNMNoym/hNhoMMd/so`      
+     `os/mMmoymMMNyyMNys/+:++ohdo:.`                          ``-/sdy//+o/++sdNMMN-ysooNMMm/ss`     
+    `ss/NMdhdyooysdMdo++/+/+/dh/-``                             `.:+my+/+/+/o+yNMm+NNmhs+mMN/os`    
+    oy:mMd+smMdmhmNd++++/+/+dh/-`                                `.-+ms+++o/+ooomMNhoo:hmomMN/so    
+   :h-dMmoNMMMoyMNho/+so/+/sm/-``.:/+o++:-`            `.-/+oo+/-.`.-omo+/++s++/sdMhoNmoMmoNMm:h/   
+  .d-yMMysNMMNomNsos+dd+/+/ds-./yhyo+/+oyyyo.         :syhso+++syys:.:dy+++/ddo+oodMyyMdNMhdMMh-d.  
+  so/MMMN++ossdMhoyodMso+++N/.`++-`     `.:+.         :+:`     `.:+:`.sdo/o+sMmsdosmMyNMMMMMMMM++s  
+ .d.mMMMMMNmNMMdosNoNm+os+oN-.`.:++o++:.`                 `./+oo++:``.+m+/y+omN+Ny+sNMMMMMMMMMMN.d- 
+ oo/MMMMMMMMMMNyohMhdhhms+sN.`-:.```-NMsoo-`          `./ooyMd````./``+m++smdydoMmo+hMMMMMMMMMMM+os 
+`d-hMMMMMMMMMMhoyhMdyNms++sN.``     `mM- ./+/`       .++:` oMh      ``+m+++sdNysMmh+sNMMMMMMMMMMd-d`
+.d.NMMmhyhNMMMsoNyNdNh+ho+oN-```   ``yMy    `:`     .-`   `dM/``   ```+d/+oh/hNhNyNsodMMMmhddNMMN.d-
+/y-NMm+:::omMNyoMdsNhodmo/+N-`./.-+syhmmso/.`         `-/oymdyys/.-:``odo+/mmsyNdhMd/dMMy/://sNMM:y/
+os:MMh///++dMmooNMhhsNNo+/+N:.:ohhhhddddhhhhy.       :hhhhhdddhdhhy+-.sd/+/sNMshsMMy+yMMs+::ssNMM/os
+ss:MMMmdhdNMMmssmMMyNMho++oN/.-..-+yyhyyhhdho.       -ydhyhyyhyy/-..-.yho+/ohMNyNMmsoyMMMdsydNMMM+os
+os:MMMMMMMMMMmo+mhMdMd+m/+/ds`     `oMm++/-``         `.:/+oNN:`    `.ds+/+d+yMhMmhy+yMMMMMMMMMMM/so
+/h-NMMMMMMMMMNsoNyyNN+hM/+/yh`      +Mh                    `MN-     `-m++/oNd/NNhsNy+hMMMMMMMMMMN-y/
+.d.mMMMMNNmdMMyshMdsmyNMs+/oN.``    -Nh`   `           `   .md`    ``/m+/ooNNydydNmoomMMMMMMMMMMN.d-
+`d-yMMdooo:/MMdoodMNyhMmoo//do.s-`   ```.:/.          `-/-.```   ./o`hy+/oomMdsNMNsosMMhMMMMNMMMh-d`
+ oo:MMMMm+sNMMNyosmNNdMhsm+/sm-:yyysssyhhh+`.`       `-`yhhyysssyys-+moo/msdMdNMmyoodMNoMhNNoMMM/oo 
+ .d.dMMm-+ssyMMmsodydmNdoMy//yd::yhdhhhhhhhshs/`  `.+yhyhhhhhhhdhs-/mo++hNshNmmyds+yMMyymoMydMMm.d. 
+  oo:NMmyhyyhNMMdosmmhhdsMN/yoyd/:shdddhhhhdhhhhysyhhhhhhhhhhddho-+dsossMMsdhydNyosNMMdyo+soMMN/os  
+  `d-sMMMoyddydMMhoomNmhyNMosmyhh/-ohddddddhhhhhhhhhhhhhddddddh+-+dshN+hMNsdmNmy+ymMNsNMMNmNMMy-d.  
+   :d-hMMoNMMN+NMMdoyhmNmdNhoNNyyh+-/hddddmmmmmmmmmmmmmmmddddy::+dymMd+NNdNNmhyosmMMydmyMMMMMd-h:   
+    +y-dMmshyoyMMMMdyhddddmNydMNyhd+::oshdddddddddddddddddyoo::omsmMModNddhhdhoyNMMm+y+mMMMMm:so    
+    `os-mMNmdNMMMMMMNyymNmdyhsdMNsym+:::--+syhddddddhhys/-::-/sdodMMysyyhmNmyshNMNNNNdyodMMm:ss`    
+     `os-dMMMMMMMMMMMMdhymNMNmhdNNohNs:---```.--::--..``.:--/ydshNmddmNMNdyshmMMMs+yooohMMd:so`     
+       +y-yMMMMMMMNmmMMNhosdNNNmmNNshNh/-..`           `..-+mhymNmmNNNmhoosmMMNNMm/syNNMMh:yo`      
+        /y:+NMMMdNhohyNMMNdshdmddddmhydms:.`           `./ydyydmhddddhsshmMMNdysNNm:mMMNo/y/        
+         .so:hMNssNsdhoNmMMNmdddNNNNNmdmmms:`         ./ymmdmNNNNNmdyyhNMMMMsydyy+dmNMd/oy.         
+          `/y/+dNhyh/MMM:hNNMMNddhyyhdhhyhyyso:-...-/oysyyhhhhhysyymNMMMMMMMdhhmdyNMm+/y+
+                                  THIS TOOL IS CREATED BY CLAY
+''')
 from random import *
 Guess=""
 password=input("password? ")
